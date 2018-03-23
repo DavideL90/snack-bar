5d0cd4c43a60e8e83fe5911efc8b6f03a4e86e10
DavideL90 <davide.lecci@icloud.com>
2018-03-23 10:05:21 +0100

    finito primo es

---
+++ Es 1/main.js
@@ -0,0 +1,9 @@
+var num1 = parseInt(prompt("Inserisci un numero:"));
+var num2 = parseInt(prompt("Inserisci un numero:"));
+if(num1 > num2){
+  document.write("Il primo numero "+ num1 +" è maggiore del secondo " + num2);
+}
+else{
+  document.write("Il primo numero "+ num1 +" è minore del secondo " + num2);
+
+}
